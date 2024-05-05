f118a226bd97739c0d433b9c49b1a2e906cc8e44
ihsanAltunsk <altnskihsan@gmail.com>
2024-05-05 19:08:58 +0300

    DENEM

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
                 "junit:target/xml-report/cucumber.xml",
                 "rerun:target/failedRerun.txt"
         },
-        fegies = {"src/test/resources/features"},
+        feg ies = {"src/test/resources/features"},
         glue = {"stepdefinitions", "hooks"},
         tags = "@API-DB",
         dryRun = false
